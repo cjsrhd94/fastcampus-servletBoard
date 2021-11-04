bc5f2b70068ea08e1cf5c74107f768ddb9443a32
cjsrhd94 <cjsrhd94@gmail.com>
2021-11-05 00:23:56 +0900

    feat: 글 수정 기능 구현

---
+++ src/com/fastcampus/controller/board/UpdateBoardServlet.java
@@ -0,0 +1,36 @@
+package com.fastcampus.controller.board;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fastcampus.biz.board.BoardDAO;
+import com.fastcampus.biz.board.BoardVO;
+
+public class UpdateBoardServlet extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 사용자 입력정보 추출
+		request.setCharacterEncoding("EUC-KR");
+		String seq = request.getParameter("seq");
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		
+		// 2. DB 연동 처리
+		BoardVO vo = new BoardVO();
+		vo.setSeq(Integer.parseInt(seq));
+		vo.setTitle(title);
+		vo.setContent(content);
+		
+		BoardDAO boardDAO = new BoardDAO();
+		boardDAO.updateBoard(vo);
+		
+		// 3. 화면 네비게이션
+		response.sendRedirect("getBoardList.do");
+	}
+
+}
