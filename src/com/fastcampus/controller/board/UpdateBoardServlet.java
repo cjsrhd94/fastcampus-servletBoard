bf38d12b4f63a7da89d30fd4b89158856061bbc4
cjsrhd94 <cjsrhd94@gmail.com>
2021-11-05 17:36:35 +0900

    feat: CharacterEncodingFilter 기능 구현

--- src/com/fastcampus/controller/board/UpdateBoardServlet.java
+++ src/com/fastcampus/controller/board/UpdateBoardServlet.java
@@ -2,10 +2,7 @@ package com.fastcampus.controller.board;
 
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,19 +13,8 @@ import com.fastcampus.biz.board.BoardVO;
 @WebServlet(urlPatterns = "/updateBoard.do")
 public class UpdateBoardServlet extends HttpServlet {
 
-	private String boardEncoding;
-
-//	@Override
-//	public void init(ServletConfig config) throws ServletException {
-//		boardEncoding = config.getInitParameter("encoding");
-//	}
-
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 1. 사용자 입력정보 추출
-		ServletContext context = getServletContext();
-		boardEncoding = context.getInitParameter("encoding");
-
-		request.setCharacterEncoding(boardEncoding);
 		String seq = request.getParameter("seq");
 		String title = request.getParameter("title");
 		String content = request.getParameter("content");
---
+++ src/com/fastcampus/controller/common/CharacterEncodingFilter.java
@@ -0,0 +1,31 @@
+package com.fastcampus.controller.common;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class CharacterEncodingFilter implements Filter {
+
+    private String boardEncoding;
+
+    public CharacterEncodingFilter() {
+        System.out.println("===> CharacterEncodingFilter 생성");
+   }
+
+    @Override
+    public void init(FilterConfig fConfig) throws ServletException {
+        boardEncoding = fConfig.getInitParameter("encoding");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // 모든 서블릿이 실행되기 전에 인코딩을 일괄적으로 처리한다.
+        request.setCharacterEncoding(boardEncoding);
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
